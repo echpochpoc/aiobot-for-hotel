dbd3d65a3f424823c04019589e5293a908439b29
Egor Govorov <impossibles5_2003@mail.ru>
2023-03-20 21:33:55 +0300

    default settings

--- core/settings.py
+++ core/settings.py
@@ -1,6 +1,6 @@
-BOT_TOKEN = '6201680674:AAFfZfD6zmsw-ft0naT_Q4yyESGlOtWdioU'
-ADMIN_ID = 334777217 #Ваш id от телеграма
-URL_DOMAIN = 'https://d37f-85-174-125-233.eu.ngrok.io'
+BOT_TOKEN = 'BOT_TOKEN'
+ADMIN_ID = 1 #Ваш id от телеграма
+URL_DOMAIN = ''
 URL_PATH = '' # Оставить как есть
 SERVER_HOST = '0.0.0.0'
 SERVER_PORT = 5000 # Порт на который вы передаете запрос от ngrok
