df7806108fc11f5c14c84db281c7d4e82705c2c8
echpochpok <61898742+echpochpok@users.noreply.github.com>
2023-03-27 13:49:44 +0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -1,12 +1,12 @@
-BOT_TOKEN = '6006512670:AAFScBj39jNkxLiILgI-0kgU3jVPeHVacSE'
-ADMIN_ID = 5062666986
-URL_DOMAIN = 'https://9a30-94-233-51-235.eu.ngrok.io'
+BOT_TOKEN = 'Token'
+ADMIN_ID = 1 #Ваш id от телеги
+URL_DOMAIN = 'Url ngrok'
 URL_PATH = ''
 SERVER_HOST = '0.0.0.0'
 SERVER_PORT = 5000
 
 DATABASE_USER = 'postgres'
-DATABASE_PASSWORD = '76258'
+DATABASE_PASSWORD = 'password'
 DATABASE_HOST = 'localhost'
 DATABASE_PORT = '5432'
-DATABASE_NAME = 'sqlaclchemy_test'
+DATABASE_NAME = 'db_name'
